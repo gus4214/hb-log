f15b15b8e0b4b14d6f1ec2fdd5fab86f7135e6f4
gus4214 <98795239+gus4214@users.noreply.github.com>
2025-01-21 16:14:41 +0900

    🐳 chore: eslint rule 추가
    prettierignore 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,34 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')];
+const eslintConfig = [
+	...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+	{
+		files: ['**/*.js', '**/*.ts', '**/*.tsx'], // 규칙이 적용될 파일 패턴
+		rules: {
+			'no-console': 'warn',
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'react/display-name': 'off',
+			'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+			'@typescript-eslint/no-unused-vars': 'off',
+			'unused-imports/no-unused-imports': 'warn',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'simple-import-sort/exports': 'warn',
+		},
+		globals: {
+			React: true,
+			JSX: true,
+		},
+	},
+];
 
 export default eslintConfig;

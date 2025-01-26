bcbdf7b7278095ce706bc829afb6efc8c24af057
gus4214 <98795239+gus4214@users.noreply.github.com>
2025-01-26 16:04:33 +0900

    🐳 chore: next-themes 설치 #3
    - ThemeProvider 컴포넌트 레이아웃 배치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 import MainLayout from '@/components/layouts/MainLayout';
+import { ThemeProvider } from '@/components/provider/ThemeProvider';
 
 export const metadata: Metadata = {
 	title: 'HB log',
@@ -13,7 +14,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 	return (
 		<html lang='en' suppressHydrationWarning>
 			<body>
-				<MainLayout>{children}</MainLayout>
+				<ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+					<MainLayout>{children}</MainLayout>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
